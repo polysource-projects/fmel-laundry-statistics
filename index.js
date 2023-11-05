ab6ce951f290614d602cb905d6c8d66cf9714b21
Androz2091 <androz2091@gmail.com>
2023-11-05 11:12:58 +0100

    format fix stats

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const queryApi = client.getQueryApi(org);
 
 const simpleQuery = `
 from(bucket: "${bucket}")
-  |> range(start: -40d)
+  |> range(start: -40d, stop: -2d)
   |> sort(columns: ["_time"])
 `;
 
@@ -37,6 +37,10 @@ const formatMachineId = (machineId) => {
 	const idx = machineId.split(' ')[0];
 	const letter = machineId.split(' ')[2];
 	return `Atrium ${idx} (${letter})`;
+}
+
+const sumOfUses = (uses) => {
+	return uses.reduce((acc, val) => acc + val, 0);
 }
 
 queryApi.queryRows(simpleQuery, {
@@ -66,7 +70,6 @@ queryApi.queryRows(simpleQuery, {
 				const date = new Date(row._time);
 				const resetDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
-
 				// Calcul les tranches d'utilisation de chaque machine
 
 				const machineId = formatMachineId(row.machine_id);
@@ -76,11 +79,13 @@ queryApi.queryRows(simpleQuery, {
 				}
 
 				if (!machinesUses[machineId][resetDate.toISOString()]) {
-					machinesUses[machineId][resetDate.toISOString()] = 0;
+					machinesUses[machineId][resetDate.toISOString()] = [];
 				}
 
 				if (row._value == 1) {
-					machinesUses[machineId][resetDate.toISOString()] += 2;
+					machinesUses[machineId][resetDate.toISOString()].push(2);
+				} else {
+					machinesUses[machineId][resetDate.toISOString()].push(0);
 				}
 
 				// Maintenant, on veut compter le nombre de dimanche, de lundi, etc. qui passent
@@ -96,134 +101,99 @@ queryApi.queryRows(simpleQuery, {
 
 			});
 
-			// Une fois qu'on a toutes les tranches d'utilisation de chaque machine
-			// On veut les sommer par jour
+		// On supprime la machine Atrium 6 (F)
+		// elle était en panne donc elle fausse les résultats
+		delete machinesUses['Atrium 6 (F)'];
 
-			const hourCountPerDayOfWeek = {};
+		// Une fois qu'on a toutes les tranches d'utilisation de chaque machine
+		// On veut les sommer par jour
 
-			Object.keys(machinesUses).forEach((machineId) => {
-				Object.keys(machinesUses[machineId]).forEach((dateS) => {
-					const date = new Date(dateS);
-					const resetDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-					if (!hourCountPerDayOfWeek[resetDate.getDay()]) {
-						hourCountPerDayOfWeek[resetDate.getDay()] = 0;
-					}
-					hourCountPerDayOfWeek[resetDate.getDay()] += machinesUses[machineId][dateS] / 60;
-				});
-			});
+		const hourCountPerDayOfWeek = {};
 
-			// moyenne par jour
-			const weekOfDaysNames = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
-			Object.keys(hourCountPerDayOfWeek).forEach((day) => {
-				console.log(weekOfDaysNames[day] + ' : ' + (hourCountPerDayOfWeek[day] / countPerDays[day]).toFixed(2) + 'h');
-			});
-			console.log(machinesUses);
-			console.log(countPerDays);
-			console.log(hourCountPerDayOfWeek);
-
-			console.log(chalk.yellow(`Processed ${rawData.length} points from InfluxDB.`));
-
-			console.log('\n');
-
-			for (let i = 1; i <= 7; i++) {
-				const weekOfDayIndex = i % 7;
-				const weekOfDay = weekOfDaysNames[weekOfDayIndex];
-
-				const averageHourCount = Math.round(hourCountPerDayOfWeek[weekOfDayIndex] / countPerDays[weekOfDayIndex]);
-				const totalHourCount = (22 - 7);
-				const totalMachinesAvailable = Object.keys(machinesUses).length;
-				const totalHourCountForAllMachines = totalHourCount * totalMachinesAvailable;
-				const percentage = Math.round(averageHourCount / totalHourCountForAllMachines * 100);
-
-				const color = percentage > 50 ? percentage > 100 ? 'red' : 'yellow' : 'green';
-
-				console.log(`Les machines à laver sont utilisées, en moyenne, ${averageHourCount}h le ${chalk.cyan(weekOfDay)} (${chalk[color](percentage)}%)`);
-
-			}
-
-			console.log('\n');
-
-			let batF = 0;
-			let batG = 0;
-
-			let machinesBatF = Object.keys(machinesUses).filter((machineId) => machineId.includes('F')).length;
-			let machinesBatG = Object.keys(machinesUses).filter((machineId) => machineId.includes('G')).length;
-
-			for (let i = 0; i < Object.keys(machinesUses).length; i++) {
-				const machineId = Object.keys(machinesUses)[i];
-				const machineUses = machinesUses[machineId];
-				const hourUsedTotal = Math.round(Object.values(machineUses).reduce((acc, val) => acc + val, 0) / 60 * 100) / 100;
-				const hourUsedPerDay = Math.ceil(hourUsedTotal / countedDates.length * 100) / 100;
-				const totalHourCount = (22 - 7);
-				const percentage = Math.round((hourUsedPerDay) / totalHourCount * 100);
-				const color = percentage > 50 ? percentage > 100 ? 'red' : 'yellow' : 'green';
-				if (machineId.includes('F')) {
-					batF += hourUsedTotal;
-				} else {
-					batG += hourUsedTotal;
+		Object.keys(machinesUses).forEach((machineId) => {
+			Object.keys(machinesUses[machineId]).forEach((dateS) => {
+				const date = new Date(dateS);
+				const resetDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+				if (!hourCountPerDayOfWeek[resetDate.getDay()]) {
+					hourCountPerDayOfWeek[resetDate.getDay()] = 0;
 				}
-				console.log(`La machine à laver ${chalk.magenta(machineId)} est indisponible ${hourUsedPerDay}h chaque jour (${chalk[color](percentage)}%)`);
+				hourCountPerDayOfWeek[resetDate.getDay()] += sumOfUses(machinesUses[machineId][dateS]) / 60;
+			});
+		});
+
+		// moyenne par jour
+		const weekOfDaysNames = ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+		Object.keys(hourCountPerDayOfWeek).forEach((day) => {
+			console.log(weekOfDaysNames[day] + ' : ' + (hourCountPerDayOfWeek[day] / countPerDays[day]).toFixed(2) + 'h');
+		});
+		console.log(machinesUses);
+		console.log(countPerDays);
+		console.log(hourCountPerDayOfWeek);
+
+		console.log(chalk.yellow(`Processed ${rawData.length} points from InfluxDB.`));
+
+		console.log('\n');
+
+		for (let i = 1; i <= 7; i++) {
+			const weekOfDayIndex = i % 7;
+			const weekOfDay = weekOfDaysNames[weekOfDayIndex];
+
+			const averageHourCount = Math.round(hourCountPerDayOfWeek[weekOfDayIndex] / countPerDays[weekOfDayIndex]);
+			const totalHourCount = (22 - 7);
+			const totalMachinesAvailable = Object.keys(machinesUses).length;
+			const totalHourCountForAllMachines = totalHourCount * totalMachinesAvailable;
+			const percentage = Math.round(averageHourCount / totalHourCountForAllMachines * 100);
+
+			const color = percentage > 50 ? percentage > 100 ? 'red' : 'yellow' : 'green';
+
+			console.log(`Les machines à laver sont utilisées, en moyenne, ${averageHourCount}h le ${chalk.cyan(weekOfDay)} (${chalk[color](percentage)}%)`);
+
+		}
+
+		console.log('\n');
+
+		let hourUsedTotalF = 0;
+		let hourUsedTotalG = 0;
+
+		const machinesBatF = Object.keys(machinesUses).filter((machineId) => machineId.includes('F'));
+		const machinesBatG = Object.keys(machinesUses).filter((machineId) => machineId.includes('G'));
+
+		const machinesBatFCount = machinesBatF.length;
+		const machinesBatGCount = machinesBatG.length;
+
+		for (let i = 0; i < Object.keys(machinesUses).length; i++) {
+			const machineId = Object.keys(machinesUses)[i];
+			const machineUses = machinesUses[machineId];
+			const hourUsedTotal = Math.round(Object.values(machineUses).reduce((acc, val) => acc + sumOfUses(val), 0) / 60 * 100) / 100;
+			const hourUsedPerDay = Math.ceil(hourUsedTotal / countedDates.length * 100) / 100;
+			const totalHourCount = (22 - 7);
+			const percentage = Math.round((hourUsedPerDay) / totalHourCount * 100);
+			const color = percentage > 50 ? percentage > 100 ? 'red' : 'yellow' : 'green';
+			if (machineId.includes('F')) {
+				hourUsedTotalF += hourUsedTotal;
+			} else {
+				hourUsedTotalG += hourUsedTotal;
 			}
+			console.log(`La machine à laver ${chalk.magenta(machineId)} est indisponible ${hourUsedPerDay}h chaque jour (${chalk[color](percentage)}%)`);
+		}
 
-			console.log('\n');
+		console.log('\n');
 
-			const totalHourCountF = (22 - 7) * machinesBatF;
-			const totalHourCountG = (22 - 7) * machinesBatG;
+		const totalHourCountF = (22 - 7) * machinesBatFCount;
+		const totalHourCountG = (22 - 7) * machinesBatGCount;
 
-			const hourUsedPerDayF = Math.ceil(batF / countedDates.length);
-			const hourUsedPerDayG = Math.ceil(batG / countedDates.length);
+		const hourUsedPerDayF = Math.ceil(hourUsedTotalF / countedDates.length);
+		const hourUsedPerDayG = Math.ceil(hourUsedTotalG / countedDates.length);
 
-			const percentageF = Math.round((hourUsedPerDayF) / totalHourCountF * 100);
-			const percentageG = Math.round((hourUsedPerDayG) / totalHourCountG * 100);
+		const percentageF = Math.round((hourUsedPerDayF) / totalHourCountF * 100);
+		const percentageG = Math.round((hourUsedPerDayG) / totalHourCountG * 100);
 
-			const colorF = percentageF > 50 ? percentageF > 100 ? 'red' : 'yellow' : 'green';
-			const colorG = percentageG > 50 ? percentageG > 100 ? 'red' : 'yellow' : 'green';
+		const colorF = percentageF > 50 ? percentageF > 100 ? 'red' : 'yellow' : 'green';
+		const colorG = percentageG > 50 ? percentageG > 100 ? 'red' : 'yellow' : 'green';
 
-			console.log(`La buanderie F est indisponible ${hourUsedPerDayF}h chaque jour (${chalk[colorF](percentageF)}%)`);
-			console.log(`Le buanderie G est indisponible ${hourUsedPerDayG}h chaque jour (${chalk[colorG](percentageG)}%)`);
+		console.log(`La buanderie F est indisponible ${hourUsedPerDayF}h chaque jour (${chalk[colorF](percentageF)}%)`);
+		console.log(`Le buanderie G est indisponible ${hourUsedPerDayG}h chaque jour (${chalk[colorG](percentageG)}%)`);
 
-
-		/*
-
-    const hourUsagePerDay = {}; // { machine_id: [{ date: Date, hourCount: Number }] }
-
-    Object.keys(usagePeriods).forEach((machineId) => {
-      const machineUsagePeriods = usagePeriods[machineId];
-      machineUsagePeriods.forEach((usagePeriod) => {
-        const start = new Date(usagePeriod.start);
-        const end = new Date(usagePeriod.end || new Date());
-        const date = new Date(start.getFullYear(), start.getMonth(), start.getDate());
-        const usage = (end - start) / 1000 / 60 / 60;
-        const hourUsage = { date, usage };
-        if (!hourUsagePerDay[machineId]) {
-          hourUsagePerDay[machineId] = [];
-        }
-        if (!hourUsagePerDay[machineId][date.toISOString()]) {
-          hourUsagePerDay[machineId][date.toISOString()] = 0;
-        }
-        hourUsagePerDay[machineId][date.toISOString()] += usage;
-      });
-    });
-
-    console.log(hourUsagePerDay);
-
-    const weekOfDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-    const weekOfDaysCount = {};
-    const dayUsagePerDay = {}; // {Sunday: hourCount, Monday: hourCount, ...}
-
-    Object.keys(hourUsagePerDay).forEach((machineId) => {
-      const machineHourUsagePerDay = hourUsagePerDay[machineId];
-      Object.keys(machineHourUsagePerDay).forEach((date) => {
-        const day = new Date(date).getDay();
-        if (!dayUsagePerDay[weekOfDays[day]]) {
-          dayUsagePerDay[weekOfDays[day]] = 0;
-        }
-        dayUsagePerDay[weekOfDays[day]] += machineHourUsagePerDay[date];
-      });
-    });
-
-    console.log(dayUsagePerDay);*/
 
 	},
 });
